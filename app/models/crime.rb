70f6f860c9c689b4af4ed1486b933498a311697c
JY Park <jeyoungp@andrew.cmu.edu>
2018-09-22 17:37:48 -0400

    scopes inactive, active

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,3 +1,11 @@
 class Crime < ApplicationRecord
+    #relationships
     has_many :investigation
+
+
+    #scopes 
+    scope :alphabetical, -> { order('name') }
+    scope :active,       -> { where(active: true) }
+    scope :inactive,     -> { where(active: false) }
+
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,8 @@
 class Unit < ApplicationRecord
     has_many :officer 
+
+    scope :active,       -> { where(active: true) }
+    scope :inactive,     -> { where(active: false) }
+    scope :alphabetical, -> { order('name') }
+
 end
